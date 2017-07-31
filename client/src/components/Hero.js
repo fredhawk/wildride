fa76143a3c304e9b58df47c8655b861288b0823f
osycon <fredrichawk@gmail.com>
2017-07-31 07:44:49 +0200

    Add styling to tagline

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -5,20 +5,24 @@ const Hero = () => {
   return (
     <section className="Hero">
       <div className="Hero-wrapper">
-        <h1 className="Hero-title">Travel. Work.</h1>
-        <h2 className="Hero-title__see-world">See the world</h2>
-        <div className="Hero-navigation">
-          <a href="#" className="Hero-btn Hero-btn-explore">
-            Explore all jobs
-          </a>
-          <a href="#" className="Hero-btn Hero-btn-employer">
-            Hiring ? Start here
-          </a>
+        <div className="Hero-container">
+          <h1 className="Hero-title">Travel. Work.</h1>
+          <h2 className="Hero-title__see-world">See the world</h2>
+          <div className="Hero-navigation">
+            <a href="#" className="Hero-btn Hero-btn-explore">
+              Explore all jobs
+            </a>
+            <a href="#" className="Hero-btn Hero-btn-employer">
+              Hiring ? Start here
+            </a>
+          </div>
+          <div className="Hero-tagline">Use Jobbatical to fin a tech, business or creative job abroad.</div>
         </div>
-        <div className="Hero-tagline">Use Jobbatical to fin a tech, business or creative job abroad.</div>
-        <p className="Hero-availability">
-          Currently <strong> 48 teams </strong> in <strong>27 countries</strong> could use your help
-        </p>
+        <div>
+          <p className="Hero-availability">
+            Currently <strong> 48 teams </strong> in <strong>27 countries</strong> could use your help
+          </p>
+        </div>
       </div>
     </section>
   );
