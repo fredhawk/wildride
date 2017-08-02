7bd1ce5df2d1c5b4d4fc17dfe5af4e702d4711c7
osycon <fredrichawk@gmail.com>
2017-08-02 11:48:53 +0200

    Add matches count Add styling to matches count

--- client/src/components/ExploreJobs.js
+++ client/src/components/ExploreJobs.js
@@ -3,11 +3,17 @@ import JobList from './JobList';
 import './ExploreJobs.css';
 
 const ExploreJobs = props => {
+  const matches = props.jobs.length;
   return (
     <section className=".ExploreJobs">
       <h1 className="ExploreJobs-title">Explore Jobs</h1>
       <div className="ExploreJobs-search">Search</div>
       <JobList jobs={props.jobs} />
+      <div className="ExploreJobs-matches">
+        <span>
+          Found {matches} matches
+        </span>
+      </div>
     </section>
   );
 };
