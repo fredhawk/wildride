efd87dccca8d8ab8a0796f5996ae01ea29143fc9
osycon <fredrichawk@gmail.com>
2017-08-02 11:21:34 +0200

    Rename Featuredlist to featuredjobs

--- client/src/components/FeaturedJobs.js
+++ client/src/components/FeaturedJobs.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import JobList from './JobList';
-import './FeaturedList.css';
+import './FeaturedJobs.css';
 
-const FeaturedList = props => {
+const FeaturedJobs = props => {
   const featured = props.jobs.filter((job, index) => index < 6);
   return (
-    <section className=".FeaturedList">
-      <h1 className="FeaturedList-title">Featured jobs</h1>
+    <section className=".FeaturedJobs">
+      <h1 className="FeaturedJobs-title">Featured jobs</h1>
       <JobList jobs={featured} />
     </section>
   );
 };
 
-export default FeaturedList;
+export default FeaturedJobs;
