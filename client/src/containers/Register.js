a1a08df749ffe0f302c11d4081f24e8a086e64f0
osycon <fredrichawk@gmail.com>
2017-08-06 19:19:14 +0200

    Add register component

---
+++ client/src/containers/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+  render() {
+    return (
+      <section className="Register">
+        <h2 className="Register__title">Register</h2>
+      </section>
+    );
+  }
+}
+
+export default Register;
