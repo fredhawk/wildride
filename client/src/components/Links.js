11a74cbfd6a2b20f4df3318b17c1eabe1671cc18
Linus <linus@ronaldjacob.de>
2017-08-11 20:47:51 +0200

    Changed design for "Follow Us" section on small screens
    Only icons are shown in smallest layout

--- client/src/components/Links.js
+++ client/src/components/Links.js
@@ -40,13 +40,20 @@ const Links = () => {
 	  <div className="title-wrapper">
 		<strong className="link-title">FOLLOW US</strong>
 	  </div>
-		<ul>
-		<li><a href="#">FACEBOOK <i className="fa fa-facebook-official"></i></a></li>
-		<li><a href="#">TWITTER <i className="fa fa-twitter-square"></i></a></li>
-		<li><a href="#">LINKEDIN <i className="fa fa-linkedin-square"></i></a></li>
-		<li><a href="#">GOOGLE+ <i className="fa fa-google-plus-square"></i></a></li>
-		<li><a href="#">INSTAGRAM <i className="fa fa-instagram"></i></a></li>
+		<ul className="social-media-text">
+			<li><a href="#">FACEBOOK <i className="fa fa-facebook-official"></i></a></li>
+			<li><a href="#">TWITTER <i className="fa fa-twitter-square"></i></a></li>
+			<li><a href="#">LINKEDIN <i className="fa fa-linkedin-square"></i></a></li>
+			<li><a href="#">GOOGLE+ <i className="fa fa-google-plus-square"></i></a></li>
+			<li><a href="#">INSTAGRAM <i className="fa fa-instagram"></i></a></li>
 		</ul>
+		<div className="social-media-icons">
+			<a href="#"><i className="fa fa-facebook-official fa-2x"></i></a>
+			<a href="#"><i className="fa fa-twitter-square fa-2x"></i></a>
+			<a href="#"><i className="fa fa-linkedin-square fa-2x"></i></a>
+			<a href="#"><i className="fa fa-google-plus-square fa-2x"></i></a>
+			<a href="#"><i className="fa fa-instagram fa-2x"></i></a>
+		</div>
       </div>
     
     </div>
