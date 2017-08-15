7e0ca29d011eadb1efc8db47454dc9aae8af3053
osycon <fredrichawk@gmail.com>
2017-08-15 07:59:23 +0200

    Add connection to the backend

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get(`/`, (res, req) => {
+mongoose.connect(process.env.DATABASE, {
+  useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', err => {
+  console.error(`Error: ${err.message}`);
+});
+require('./models/User');
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get(`/`, (req, res) => {
   res.send(`Hey there`);
+});
+app.options('/register', cors());
+app.post('/register', cors(), (req, res) => {
+  console.log(req.body);
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log('Received post: %s %s %s', name, email, password);
+  res.send('got it!');
 });
 
 const server = app.listen(PORT, () => {
