33b92897388fde6690e2b112d3911d2835064624
osycon <fredrichawk@gmail.com>
2017-08-17 06:55:20 +0200

    Fix signing up now returns something so it doesn't time out.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,6 +3,6 @@ const User = mongoose.model('User');
 const promisify = require('es6-promisify');
 
 exports.signup = async (req, res, next) => {
-  console.log(req.body);
   const user = await new User(req.body).save();
+  res.json(req.body);
 };
