f5a30560ba3bb85038c298ee6ceb063cc3126e4f
osycon <fredrichawk@gmail.com>
2017-08-20 16:03:35 +0200

    Removed uneeded code

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,41 +1,8 @@
 import React from 'react';
-// import React, { Component } from 'react';
 import './App.css';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Links from '../components/Links';
-<<<<<<< HEAD
-import ExploreJobs from './ExploreJobs';
-import Register from './Register';
-import PostJob from './PostJob';
-import { jobs } from '../models/data.json';
-import Mission from '../components/Mission';
-
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      jobs
-    };
-  }
-
-  render() {
-    return (
-      <div className="App">
-	  
-        <Register />
-        <Hero />
-        <FeaturedJobs jobs={this.state.jobs} />
-		<ExploreJobs jobs={this.state.jobs} /> 
-		<Mission />
-        <Links />
-        <PostJob />
-      </div>
-	  );
-  }
-}
-=======
 
 const App = () => {
   return (
@@ -46,6 +13,5 @@ const App = () => {
     </div>
   );
 };
->>>>>>> feature/navigation
 
 export default App;
