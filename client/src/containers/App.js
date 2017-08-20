d10125187cb1dea27f93152a0dd3764b57a044c1
osycon <fredrichawk@gmail.com>
2017-08-20 15:59:10 +0200

    Cleanup comments

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -4,24 +4,6 @@ import './App.css';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Links from '../components/Links';
-// import { jobs } from '../models/data.json';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       jobs
-//     };
-//   }
-
-//   render() {
-//     return (
-
-//     );
-//   }
-// }
-
-// export default App;
 
 const App = () => {
   return (
