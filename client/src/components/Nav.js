99d705f3e5e2d84071669f6eaacd1fed3c93284f
osycon <fredrichawk@gmail.com>
2017-08-20 12:33:20 +0200

    Created initial navigation component Added react router to the project

---
+++ client/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <section className=".Navigation">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Sign Up</li>
+          <li>Log in</li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default Navigation;
