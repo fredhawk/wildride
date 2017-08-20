10f084ce79d19c3b593b4da332d5c58cc5c74ee4
Linus <linus@ronaldjacob.de>
2017-08-20 11:32:51 +0200

    Created React-component for Mission-statement

---
+++ client/src/components/Mission.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Mission.css';
+
+const Mission = () => {
+  return (
+    <section className="Mission">
+      <div className="Mission-wrapper">
+        <div className="Mission-container">
+          <h1 className="Mission-title">Our Mission</h1>
+		  <p className="Mission-statement">At Jobbatical we think beyond borders and help the
+		  world work together. Join our journey to build a borderless world where 
+		  skills are valued over passports and find out where on planet Earth* your next job should be.</p>
+		  <a href="#"><button type="button">Join</button></a>
+		  <p className="Mission-appendix">*more planets coming soon</p>
+		</div>
+      </div>
+    </section>
+  );
+};
+
+export default Mission;
