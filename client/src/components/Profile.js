2b374e9510633bb1b315fbcec536bb9c3781d7bb
Linus <linus@ronaldjacob.de>
2017-08-29 20:38:33 +0200

    Added more examples and improved styling

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -8,8 +8,24 @@ const Profile = props => {
             <h2 id="profile-name">Profile Name</h2>
             <div className="profile-info">
                 <div className="data-pair">
-                    <h4>e-Mail:</h4>
-                    <p>example@email.com</p>
+                    <h4>e-Mail:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
+                    <div className="profile-data"><p>example@email.com</p></div>
+                </div>
+                <div className="data-pair">
+                    <h4>Location:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
+                    <div className="profile-data"><p>Amsterdam</p></div>
+                </div>
+                <div className="data-pair">
+                    <h4>Date of Birth:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
+                    <div className="profile-data"><p>34.04.2012</p></div>
+                </div>
+                <div className="data-pair">
+                    <h4>Description:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
+                    <div className="profile-data"><p>I'm a very good example ... lorem ipsum bla bla bla</p></div>
+                </div>
+                <div className="data-pair">
+                    <h4>Website:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
+                    <div className="profile-data"><p></p></div>
                 </div>
             </div>
         </div>
