526362ed542030ceaf3d1a5d4c7361250f4b4c18
Linus <linus@ronaldjacob.de>
2017-08-31 14:33:23 +0200

    Changed the profile data to display correct values of the user

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -5,27 +5,27 @@ import './Profile.css';
 const Profile = props => {
     return (
         <div className="profile-container">
-            <h2 id="profile-name">Profile Name</h2>
+            <h2 id="profile-name">{props.user.name}</h2>
             <div className="profile-info">
                 <div className="data-pair">
                     <h4>e-Mail:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
-                    <div className="profile-data"><p>example@email.com</p></div>
+                    <div className="profile-data"><p>{props.user.email}</p></div>
                 </div>
                 <div className="data-pair">
                     <h4>Location:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
-                    <div className="profile-data"><p>Amsterdam</p></div>
+                    <div className="profile-data"><p>{props.user.location}</p></div>
                 </div>
                 <div className="data-pair">
                     <h4>Date of Birth:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
-                    <div className="profile-data"><p>34.04.2012</p></div>
+                    <div className="profile-data"><p>{props.user.birth}</p></div>
                 </div>
                 <div className="data-pair">
                     <h4>Description:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
-                    <div className="profile-data"><p>I'm a very good example ... lorem ipsum bla bla bla</p></div>
+                    <div className="profile-data"><p>{props.user.descr}</p></div>
                 </div>
                 <div className="data-pair">
                     <h4>Website:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
-                    <div className="profile-data"><p></p></div>
+                    <div className="profile-data"><p>{props.user.web}</p></div>
                 </div>
             </div>
         </div>
