f5ede5af44ca3d3e6d0c989762c775e5c41b78c1
Linus <linus@ronaldjacob.de>
2017-09-04 06:32:01 +0200

    Deleted pencil icons and replaced them with one Edit button at the bottom

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -10,26 +10,27 @@ class Profile extends Component {
             <h2 id="profile-name">{this.props.user.name}</h2>
             <div className="profile-info">
                 <div className="data-pair">
-                    <h4>e-Mail:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
+                    <h4>e-Mail:</h4>
                     <div className="profile-data"><p>{this.props.user.email}</p></div>
                 </div>
                 <div className="data-pair">
-                    <h4>Location:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
+                    <h4>Location:</h4>
                     <div className="profile-data"><p>{this.props.user.location}</p></div>
                 </div>
                 <div className="data-pair">
-                    <h4>Date of Birth:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
+                    <h4>Date of Birth:</h4>
                     <div className="profile-data"><p>{this.props.user.birth}</p></div>
                 </div>
                 <div className="data-pair">
-                    <h4>Description:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
+                    <h4>Description:</h4>
                     <div className="profile-data"><p>{this.props.user.descr}</p></div>
                 </div>
                 <div className="data-pair">
-                    <h4>Website:<i className="fa fa-pencil-square-o fa-lg"></i></h4>
+                    <h4>Website:</h4>
                     <div className="profile-data"><p>{this.props.user.web}</p></div>
                 </div>
             </div>
+            <button><i className="fa fa-pencil-square-o fa-lg" /> Edit</button>
         </div>
     );
 }
