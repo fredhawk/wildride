6fb524d2ef664e7c7110b831566969d526e9b2fa
Linus <linus@ronaldjacob.de>
2017-09-09 19:46:43 +0200

    Updated userSchema

--- models/User.js
+++ models/User.js
@@ -34,6 +34,10 @@ const userSchema = new Schema({
     type: String,
     trim: true
   },
+  food: {
+    type: String,
+    trim: true
+  },
   created: [
     {
       type: Schema.Types.ObjectId,
