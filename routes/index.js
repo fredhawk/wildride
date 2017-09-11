43cb3b73c2e739e1104a23e85e0d449e3b1d340b
osycon <fredrichawk@gmail.com>
2017-09-11 16:39:17 +0200

    Reorganised the routes

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,35 @@ const userController = require('../controllers/usersController');
 const meetController = require('../controllers/meetController');
 const authController = require('../controllers/authController');
 
+/*********************
+* Meetups
+**********************/
 // Get all meetups from server
 router.get('/api/meet', meetController.getMeets);
 // Get one meetup from server
 router.get('/api/meet/:id', meetController.getSingleMeet);
 // Post a new meet to server
 router.post('/api/meet/new', meetController.validateMeet, meetController.postMeet);
-
 router.post('/api/meet/attend/:id', userController.attend);
 router.post('/api/meet/unattend/:id', userController.unattend);
 
-// Signing up
-router.post('/api/register', userController.validateRegister, userController.signup, authController.login);
-// Changing profile-data
-router.post('/api/profile', userController.updateProfile);
+/*********************
+* Authentication
+**********************/
 
 router.post('/api/login', authController.login);
 router.get('/api/logout', authController.logout);
 
+/*********************
+ * User
+ **********************/
+
+router.post('/api/register', userController.validateRegister, userController.signup, authController.login);
+router.post('/api/profile', userController.updateProfile);
 router.get('/api/user/meetup', userController.getMeetup);
 router.post('/api/user/update', userController.updateProfile);
 
-// Route to test if user is logged in or not.
+// Get current users information
 router.get('/api/current_user', (req, res) => {
   res.send(req.user);
 });
