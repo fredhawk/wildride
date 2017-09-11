643d19949f284efbe6fcd8c5e6865adae4a4bfe1
osycon <fredrichawk@gmail.com>
2017-09-11 20:00:28 +0200

    Cleaned up routers

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,8 @@ const authController = require('../controllers/authController');
 /*********************
 * Meetups
 **********************/
-// Get all meetups from server
 router.get('/api/meet', meetController.getMeets);
-// Get one meetup from server
 router.get('/api/meet/:id', meetController.getSingleMeet);
-// Post a new meet to server
 router.post('/api/meet/new', meetController.validateMeet, meetController.postMeet);
 router.post('/api/meet/attend/:id', userController.attend);
 router.post('/api/meet/unattend/:id', userController.unattend);
@@ -19,22 +16,15 @@ router.post('/api/meet/unattend/:id', userController.unattend);
 /*********************
 * Authentication
 **********************/
-
 router.post('/api/login', authController.login);
 router.get('/api/logout', authController.logout);
 
 /*********************
  * User
  **********************/
-
 router.post('/api/register', userController.validateRegister, userController.signup, authController.login);
-router.post('/api/profile', userController.updateProfile);
+router.get('/api/user', userController.getUser);
 router.get('/api/user/meetup', userController.getMeetup);
 router.post('/api/user/update', userController.updateProfile);
-
-// Get current users information
-router.get('/api/current_user', (req, res) => {
-  res.send(req.user);
-});
 
 module.exports = router;
